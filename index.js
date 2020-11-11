b1ad9435156a50d4ca244501e3a460f55c4faf6e
Bachman Rocks <azfaryar004@gmail.com>
2020-11-11 19:10:04 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express"),
+	http = require("http");
+const hostname = "localhost";
+const port = 3000;
+
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const dishRouter = require("./routes/dishRouter");
+
+app.use("/dishes", dishRouter);
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
